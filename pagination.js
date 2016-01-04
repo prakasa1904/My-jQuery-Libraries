3bd71ec5af1d178eb0182296a333406e4853b879
Prakasa <na.raka45@gmail.com>
2016-01-04 09:59:13 +0700

    Fixed Bug Next Prev

--- pagination.js
+++ pagination.js
@@ -7,7 +7,7 @@
 */
 $.fn.pagination = function(options) {
   var defaults = {
-    uri: 'http://www.terpusat.com/',
+    uri: 'http://www.domain.com/',
     total:0,
     limit: 10,
     page: 1,
@@ -21,9 +21,11 @@ $.fn.pagination = function(options) {
   var settings = $.extend( {}, defaults, options );
   var navigation_html = ''; var i = 0; var current_link = 1;
 
-  var number_of_pages = Math.round(settings.total/settings.limit);
+  var number_of_pages = Math.ceil(settings.total/settings.limit);
+  console.log(number_of_pages + ' == ' + settings.total + ' == ' + settings.limit);
   if( settings.page > 1 )
     navigation_html += settings.open_tag + '<a href="' + settings.uri + '?page=1&limit=' + settings.limit + '">' + settings.prev_text + '</a>' + settings.close_tag;
+  if( number_of_pages > 1 ){
     while( number_of_pages > i){
       if( settings.page == current_link )
         navigation_html += settings.current_open_tag + '<a href="' + settings.uri + '?page=' + current_link + '&limit=' + settings.limit + '">' + (current_link) + '</a>' + settings.current_close_tag;
@@ -31,7 +33,8 @@ $.fn.pagination = function(options) {
         navigation_html += settings.open_tag + '<a href="' + settings.uri + '?page=' + current_link + '&limit=' + settings.limit + '">'+ (current_link) +'</a>' + settings.close_tag;
       i++; current_link++;
     }
-    if( settings.page != number_of_pages && number_of_pages != 0 )
+  }
+    if( settings.page < number_of_pages && number_of_pages > 1 )
         navigation_html += settings.open_tag + '<a href="' + settings.uri + '?page=' + (number_of_pages) + '&limit=' + settings.limit + '">' + settings.next_text + '</a>' + settings.close_tag;
     $(this).html(navigation_html);
 };
